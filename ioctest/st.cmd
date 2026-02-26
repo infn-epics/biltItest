f5b10c313557925e0e097ad0cc7a41ffcab92606
Andrea Michelotti <andrea.michelotti@gmail.com>
2026-02-26 14:00:42 +0100

    changed name to unimag interface for conflits

--- ioctest/st.cmd
+++ ioctest/st.cmd
@@ -13,16 +13,16 @@ drvAsynIPPortConfigure("TEST", "lel-mag-cpsu03.int.eli-np.ro:5025", 0, 0, 0)
 epicsEnvSet "STREAM_PROTOCOL_PATH", "../biltItestApp/protocol/"
 
 dbLoadRecords("$(TOP)/db/biltItest.template","device=$(PREFIX):HCOR06,port=TEST,module=1,max=10")
-dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):HCOR06,port=TEST,module=1,max=10")
+dbLoadRecords("$(TOP)/db/unimag-biltItest.db","device=$(PREFIX):HCOR06,port=TEST,module=1,max=10")
 
 dbLoadRecords("$(TOP)/db/biltItest.template","device=$(PREFIX):VCOR06,port=TEST,module=2,max=10")
-dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):VCOR06,port=TEST,module=2,max=10")
+dbLoadRecords("$(TOP)/db/unimag-biltItest.db","device=$(PREFIX):VCOR06,port=TEST,module=2,max=10")
 
 dbLoadRecords("$(TOP)/db/biltItest.template","device=$(PREFIX):HCOR07,port=TEST,module=3,max=10")
-dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):HCOR07,port=TEST,module=3,max=10")
+dbLoadRecords("$(TOP)/db/unimag-biltItest.db","device=$(PREFIX):HCOR07,port=TEST,module=3,max=10")
 
 dbLoadRecords("$(TOP)/db/biltItest.template","device=$(PREFIX):VCOR07,port=TEST,module=4,max=10")
-dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):VCOR07,port=TEST,module=4,max=10")
+dbLoadRecords("$(TOP)/db/unimag-biltItest.db","device=$(PREFIX):VCOR07,port=TEST,module=4,max=10")
 
 #
 # The circuits are complete. The engine of fate is primed. One turn of the
