4655033733fa70c1621385fd2c905f7c6c76c760
Andrea Michelotti <andrea.michelotti@gmail.com>
2026-02-26 12:05:53 +0100

    updated shows errors

--- ioctest/st.cmd
+++ ioctest/st.cmd
@@ -6,17 +6,23 @@
 dbLoadDatabase "../dbd/biltItest.dbd"
 biltItest_registerRecordDeviceDriver(pdbbase)
 
-epicsEnvSet("PREFIX", "LEL:MAG:CPSU01")
+epicsEnvSet("PREFIX", "LEL:MAG:CPSU03")
 
-
-drvAsynIPPortConfigure("TEST", "10.16.4.48:5025", 0, 0, 0)
+drvAsynIPPortConfigure("TEST", "lel-mag-cpsu03.int.eli-np.ro:5025", 0, 0, 0)
 
 epicsEnvSet "STREAM_PROTOCOL_PATH", "../biltItestApp/protocol/"
 
+dbLoadRecords("$(TOP)/db/biltItest.template","device=$(PREFIX):HCOR06,port=TEST,module=1,max=10")
+dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):HCOR06,port=TEST,module=1,max=10")
 
-dbLoadRecords("$(TOP)/db/biltItest.template","device=$(PREFIX):HCOR01, port=TEST,module=1,max=1")
+dbLoadRecords("$(TOP)/db/biltItest.template","device=$(PREFIX):VCOR06,port=TEST,module=2,max=10")
+dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):VCOR06,port=TEST,module=2,max=10")
 
-dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):HCOR01, port=TEST,module=1,max=1")
+dbLoadRecords("$(TOP)/db/biltItest.template","device=$(PREFIX):HCOR07,port=TEST,module=3,max=10")
+dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):HCOR07,port=TEST,module=3,max=10")
+
+dbLoadRecords("$(TOP)/db/biltItest.template","device=$(PREFIX):VCOR07,port=TEST,module=4,max=10")
+dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):VCOR07,port=TEST,module=4,max=10")
 
 #
 # The circuits are complete. The engine of fate is primed. One turn of the
@@ -24,4 +30,3 @@ dbLoadRecords("$(TOP)/db/unimag.db","device=$(PREFIX):HCOR01, port=TEST,module=1
 #
 iocInit()
 dbl
-
